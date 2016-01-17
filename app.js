d5cff351efcb48eb66e40619b151e9fdddca029e
cangku <cangku@unofficial.cn>
2016-01-17 11:46:53 +0800

    webhook test

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res) {
     res.send('Hello world');
 });
 
+app.get('/webhook', function(req, res) {
+    console.log(req);
+    res.send('webhook');
+});
+
 app.listen(process.env.PORT, function() {
     console.log('runing');
 })
